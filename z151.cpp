796d96a11ff6bf058c0138018711dcaed7f04ca7
ante3602 <aljinovic.ante7@gmail.com>
2022-10-14 00:19:45 +0200

    solved z1.5

---
+++ z151.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "I like to write/n before I've read it.\n\n";
+    cout << "Then, with my pen, I always edit.\n";
+    cout << "But, with computer, now I type";
+    cout << "An never, ever get it right./n";
+
+    return 0;
+}
